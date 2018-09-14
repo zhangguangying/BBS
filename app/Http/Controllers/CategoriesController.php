f2eb7d3e1dbbc23bd9514ed783f6cf3bd87e4785
zhangguangying <786781353@qq.com>
2018-09-14 09:24:58 +0000

    话题排序

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -10,9 +10,9 @@ use App\Models\Topic;
 class CategoriesController extends Controller
 {
     //
-    public function show(Category $category)
+    public function show(Category $category, Request $request)
     {
-        $topics = Topic::where('category_id', $category->id)->paginate(20);
+        $topics = Topic::where('category_id', $category->id)->withOrder($request->order)->paginate(20);
 
         return view('topics.index', compact('topics', 'category'));
     }
