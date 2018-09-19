7fa068028cadff8089fe89cc8bfda2bdcb825921
zhangguangying <786781353@qq.com>
2018-09-19 07:03:06 +0000

    集成laravel-permission扩展包

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -34,7 +34,7 @@ class TopicObserver
             dispatch(new TranslateSlug($topic));
         }
     }
-
+    
     public function deleted(Topic $topic)
     {
         \DB::table('replies')->where('topic_id', $topic->id)->delete();
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -38,5 +38,9 @@ class UsersTableSeeder extends Seeder
         $user->email = 'hyuiing@163.com';
         $user->avatar = 'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png?imageView2/1/w/200/h/200';
         $user->save();
+        $user->assignRole('Founder');
+
+        $user = User::find(2);
+        $user->assignRole('Maintainer');
     }
 }
