3f4d3670041eec84f2b73de17d5f2d98b0266c1d
zhangguangying <786781353@qq.com>
2018-09-21 02:33:40 +0000

    侧边栏资源推荐

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -7,15 +7,17 @@ use Illuminate\Http\Request;
 use App\Models\Category;
 use App\Models\Topic;
 use App\Models\User;
+use App\Models\Link;
 
 class CategoriesController extends Controller
 {
     //
-    public function show(Category $category, Request $request, User $user)
+    public function show(Category $category, Request $request, User $user, Link $link)
     {
         $topics = Topic::where('category_id', $category->id)->withOrder($request->order)->paginate(20);
 
         $active_users = $user->getActiveUsers();
+        $links = $link->getAllCached();
 
         return view('topics.index', compact('topics', 'category', 'active_users'));
     }
--- resources/views/topics/_sidebar.blade.php
+++ resources/views/topics/_sidebar.blade.php
@@ -6,6 +6,24 @@
     </div>
 </div>
 
+@if (count($links))
+    <div class="panel panel-default">
+        <div class="panel-body active-users">
+
+            <div class="text-center">资源推荐</div>
+            <hr>
+            @foreach ($links as $link)
+                <a class="media" href="{{ $link->link }}">
+                    <div class="media-body">
+                        <span class="media-heading">{{ $link->title }}</span>
+                    </div>
+                </a>
+            @endforeach
+
+        </div>
+    </div>
+@endif
+
 @if (count($active_users))
 <div class="panel panel-default">
     <div class="panel-body active-users">
